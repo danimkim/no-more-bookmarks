04e824bb558f5e623af47022ae7cc147ce31f008
danimkim <danimkim.dev@gmail.com>
2025-07-18 22:12:20 +0100

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "No More Bookmarks",
+  description:
+    "A personal archive for turning saved inspiration into real action and trackable progress.",
+  generator: "next.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
