69883dcba57d33fc910832831bf39df0571fad85
danimkim <danimkim.dev@gmail.com>
2025-07-30 03:54:50 +0100

    chore: rename the lib util file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { customStorageAdapter } from "./customStorageAdapter";
+import { customStorageAdapter } from "./custom-storage-adapter";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
